ef02e49f29962b3c6094a5667b78cd5339ec3e49
1000hyehyang <102294782+1000hyehyang@users.noreply.github.com>
2022-09-30 13:32:15 +0900

    BAEKJOON 킹, 퀸, 룩, 비숍, 나이트, 폰

--- 1. 입출력과 사칙연산/3003.java
+++ 1. 입출력과 사칙연산/3003.java
@@ -1,4 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      StringBuilder sb = new StringBuilder();
+      
+     int[] chess = {1,1,2,2,2,8};
+     
+     for(int i=0; i<6; i++) {
+         int now = Integer.parseInt(st.nextToken());
+    	 sb.append((chess[i]-now)+" ");
+     }
+        System.out.print(sb);  
+    }
+}
+
+
+
+
+/*import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,4 +36,4 @@ public class Main {
     	 }
           
     }
-}
+}*/
