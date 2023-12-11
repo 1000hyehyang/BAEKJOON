0db90ed59992d427afca59c460f8da4d3d717a34
1000hyehyang <102294782+1000hyehyang@users.noreply.github.com>
2023-12-12 04:37:15 +0900

    BAEKJOON 완전 이진 트리

---
+++ 트리/9934.py
@@ -0,0 +1,23 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+K = int(input())
+arr = list(map(int, input().split()))
+tree = [
+    []
+    for _ in range(K)
+]
+
+def makeTree(arr, x):
+    mid = (len(arr) // 2)
+    tree[x].append(arr[mid])
+    if len(arr) == 1:
+        return
+    makeTree(arr[:mid], x + 1)
+    makeTree(arr[mid + 1:], x + 1)
+
+makeTree(arr, 0)
+
+for i in range(K):
+    print(*tree[i])
