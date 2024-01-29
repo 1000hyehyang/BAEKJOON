977d59954169cf7908f044ccd569df95b9890bd8
1000hyehyang <102294782+1000hyehyang@users.noreply.github.com>
2024-01-29 15:34:10 +0900

    [Silver III] Title: N과 M (3), Time: 2736 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,19 @@
+n, m = tuple(map(int, input().split()))
+selected = []
+
+def print_answer():
+    for elem in selected:
+        print(elem, end = ' ')
+    print()
+
+def choose(cnt):
+    if cnt == m:
+        print_answer()
+        return
+    
+    for i in range(1, n + 1):
+        selected.append(i)
+        choose(cnt + 1)
+        selected.pop()
+
+choose(0)
